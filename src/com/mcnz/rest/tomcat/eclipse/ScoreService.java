29d99414552988f4599424b36c982d7324b0571c
Antonio J. Fdez <antonio-jesus.fernandez-amador@t-systems.com>
2022-02-09 13:08:17 +0100

    Cambio a la DB correcta, crea una peticion GET de los autores y se devuelve en formato JSON

--- src/com/mcnz/rest/tomcat/eclipse/ScoreService.java
+++ src/com/mcnz/rest/tomcat/eclipse/ScoreService.java
@@ -1,15 +1,46 @@
 package com.mcnz.rest.tomcat.eclipse;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.mcnz.rest.tomcat.eclipse.conexion.ConexionController;
+import com.mcnz.rest.tomcat.eclipse.entidades.Autores;
 
 @Path("/")
 public class ScoreService {
 
 	@GET
-	@Path("/score/wins")
-	@Produces("text/plain")
-	public int getWins() {
-		return Score.WINS;
+	@Path("/autores")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAutores() {
+		Autores autor = new Autores();
+		ArrayList<Autores> listaAutores = new ArrayList<>();
+		ConexionController cc = new ConexionController();
+		cc.iniciarConexion();
+
+		// Ejecutar sentencia SQL
+		ResultSet rs = cc.ejecutarSentencia("SELECT * FROM autores;");
+
+		try {
+			// Leer el ResultSet
+			while (rs.next()) {
+				//mensaje = rs.getString("ID") + " - " + rs.getString("nick") + "\n";
+				autor.setId(rs.getInt("ID"));
+				autor.setNick(rs.getString("nick"));
+				listaAutores.add(autor);
+			}
+		} catch (SQLException e) {
+			System.out.println("Error al mostrar el ResultSet");
+			e.printStackTrace();
+		}
+
+		cc.cerrarConexion();
+		return Response.ok(listaAutores, MediaType.APPLICATION_JSON).build();
 	}
 
 	@GET
--- src/com/mcnz/rest/tomcat/eclipse/conexion/ConexionController.java
+++ src/com/mcnz/rest/tomcat/eclipse/conexion/ConexionController.java
@@ -5,14 +5,45 @@ import java.sql.*;
 public class ConexionController {
 
 	private Connection conexion = null;
-	
-	public void iniciarConexion() throws SQLException {
-		conexion = DriverManager.getConnection("jdbc:sqlite:test.db");
-	}
-	
-	public void cerrarConexion() throws SQLException {
-		if (conexion != null) {
-			conexion.close();
+
+	public void iniciarConexion() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conexion = DriverManager.getConnection("jdbc:sqlite:/home/tsi/eclipse-workspace/tomcat-rest-eclipse/propuestas.db");
+			System.out.println("Conexión con la Base de Datos realizada con éxito");
+		} catch (SQLException e) {
+			System.out.println("La conexión con la Base de Datos ha fallado");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Error al cargar el driver sqlite");
+			e.printStackTrace();
 		}
+	}
+
+	public void cerrarConexion() {
+		if (conexion != null) {
+			try {
+				conexion.close();
+				System.out.println("Conexion finalizada con exito");
+			} catch (SQLException e) {
+				System.out.println("Error al desconetar con la Base de Datos");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public ResultSet ejecutarSentencia(String sentencia) {
+		Statement miStatement;
+		ResultSet rs = null;
+		if (conexion != null) {
+			try {
+				miStatement = conexion.createStatement();
+				rs = miStatement.executeQuery(sentencia);
+			} catch (SQLException e) {
+				System.out.println("Error al ejecutar la sentencia SQL");
+				e.printStackTrace();
+			}
+		}
+		return rs;
 	}
 }
